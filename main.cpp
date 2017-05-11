5567a94267a9985e776d7abdbccb7409f68c2424
bloisi@dis.uniroma1.it <bloisi@dis.uniroma1.it>
2017-05-11 20:15:41 +0200

    bug fix

--- main.cpp
+++ main.cpp
@@ -20,8 +20,9 @@ using namespace cv;
 bool ldown = false, lup = false;
 // Flags updated according to right mouse button activity
 bool rdown = false, rup = false;
-// Original image
+// current image
 Mat img;
+Mat prev_img;
 // Cropped image
 Mat crop;
 
@@ -34,6 +35,11 @@ Rect box;
 int min_width;
 //min clipping area height
 int min_height;
+
+bool is_resized;
+const int MAX_WIDTH = 1024;
+
+string window_name;
 
 //function headers
 void help();
@@ -114,12 +120,12 @@ int main(int argc, char* argv[])
     }
 	
     if (is_video) {
-	processVideo(video_name, ms);
+        processVideo(video_name, ms);
     }
     else {
         if(is_dir) {
-	    processDir(dir_name);
-	}
+	        processDir(dir_name);
+	    }
     }
 
     return EXIT_SUCCESS;
@@ -131,13 +137,14 @@ void processDir(string dir_name) {
 		
     int keyboard = -1;
 	
-    namedWindow("Cropping app");
-    setMouseCallback("Cropping app", mouse_callback);
+	window_name.append("Cropping app");
+    namedWindow(window_name);
+    setMouseCallback(window_name, mouse_callback);
 
     //read input data. ESC or 'q' for quitting
     while( (char)keyboard != 'q' && (char)keyboard != 27 )
     {		
-	box.x = 0;
+	    box.x = 0;
         box.y = 0;
         box.width = min_width;
         box.height = min_height;
@@ -158,7 +165,7 @@ void processDir(string dir_name) {
 	cout << "W: " << img.cols << "   H: " << img.rows << endl;
 		
 		
-        imshow("Cropping app", img);
+        imshow(window_name, img);
         
         keyboard = waitKey(0);
 		
@@ -203,13 +210,12 @@ void processDir(string dir_name) {
 // Callback function for mouse events
 static void mouse_callback(int event, int x, int y, int, void *)
 {
-
     // When the left mouse button is pressed, record its position and save it in corner1
     if(event == EVENT_LBUTTONDOWN)
     {
         ldown = true;
-	corner1.x = x;
-	corner1.y = y;	        
+        corner1.x = x;
+        corner1.y = y;	        
     }
     // When the left mouse button is released, record its position and save it in corner2
     if(event == EVENT_LBUTTONUP)
@@ -219,15 +225,14 @@ static void mouse_callback(int event, int x, int y, int, void *)
     // Update the box showing the selected region as the user drags the mouse
     if(ldown == true && lup == false)
     {
-        
-	corner2.x = x;
-	if (corner2.x == corner1.x) {
-	    corner2.x = corner1.x + min_width;
-	}
-	corner2.y = corner1.y + (x - corner1.x)*(min_width / min_height);
-	if (corner2.y == corner1.y) {
-	    corner2.y = corner1.y + min_height;
-	}
+        corner2.x = x;
+		float def_ratio = (min_width / min_height);
+		//(corner2.x - corner1.x) : (corner2.y - corner1.y) = def_ratio
+		//(corner2.x - corner1.x) / def_ratio = (corner2.y - corner1.y)
+		corner2.y = cvRound((corner2.x - corner1.x) / def_ratio) + corner1.y;
+        if (corner2.y == corner1.y) {
+			corner2.y = corner1.y + min_height;
+        }
 
         if(0 <= box.x &&
            0 <= box.width &&
@@ -238,9 +243,8 @@ static void mouse_callback(int event, int x, int y, int, void *)
         {
            Mat local_img = img.clone();
            rectangle(local_img, corner1, corner2, Scalar(0, 0, 255));
-           imshow("Cropping app", local_img);
-        }
-		        
+           imshow(window_name, local_img);
+        }     
     }
 
     // Define ROI and crop it out when both corners have been selected
@@ -258,22 +262,21 @@ static void mouse_callback(int event, int x, int y, int, void *)
            box.y + box.height <= img.rows)
         {
             Mat crop_current(img, box);
-
-	    if(crop_current.size().width > 0 && crop_current.size().height > 0) {
-	        namedWindow("Crop");
-	        imshow("Crop", crop_current);
-		crop = crop_current.clone();
-	    }			
+			if(crop_current.size().width > 0 && crop_current.size().height > 0) {
+                namedWindow("Crop");
+                imshow("Crop", crop_current);
+                crop = crop_current.clone();
+            }		
         }		
-	ldown = false;
+        ldown = false;
         lup = false;        
     }
 
     if(event == EVENT_RBUTTONDOWN)
     {
         rdown = true;
-	drag.x = x;
-	drag.y = y;
+        drag.x = x;
+        drag.y = y;
     }
 
 
@@ -284,21 +287,26 @@ static void mouse_callback(int event, int x, int y, int, void *)
 
     // Update the box showing the selected region as the user drags the mouse
     if(rdown == true && rup == false)
-    {
-	
-	    corner1.x -= (drag.x - x);
-	    corner1.y -= (drag.y - y);
+    {	
+		int current_width = std::abs(corner1.x - corner2.x);
+		int current_height = std::abs(corner1.y - corner2.y);
+	    int offset_x = (x - drag.x);
+	    int offset_y = (y - drag.y);
 
-	    corner2.x -= (drag.x - x);
-	    corner2.y -= (drag.y - y);
+		corner1.x += offset_x;
+		corner1.y += offset_y;
+
+		corner2.x = corner1.x + current_width;
+		corner2.y = corner1.y + current_height;
+
+		drag.x = x;
+		drag.y = y;
 
 	    Mat local_img = img.clone();
-			
 	    rectangle(local_img, corner1, corner2, Scalar(0, 0, 255));
-	    imshow("Cropping app", local_img);
+	    imshow(window_name, local_img);
 	    waitKey(30);
-	    drag.x = x;
-	    drag.y = y;
+	    
 	
     }
 
@@ -341,10 +349,8 @@ static void mouse_callback(int event, int x, int y, int, void *)
 
 void processVideo(string video_name, int ms) {
 
-#ifdef VIDEO_OK
-
-	VideoCapture cap(video_name); // open the default camera
-	if (!cap.isOpened()) {  // check if we succeeded
+	VideoCapture cap(video_name);
+	if (!cap.isOpened()) {
 		cout << "Unable to open " << video_name << endl;
 		cout << "Exiting..." << endl;
 		exit(EXIT_FAILURE);
@@ -354,9 +360,9 @@ void processVideo(string video_name, int ms) {
 		cap.set(CV_CAP_PROP_POS_MSEC, ms);
 	}
 
-	int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));     // Get Codec Type- Int form
-	// Transform from int to char via Bitwise operators
-	char EXT[] = { (char)(ex & 0XFF) , (char)((ex & 0XFF00) >> 8),(char)((ex & 0XFF0000) >> 16),(char)((ex & 0XFF000000) >> 24), 0 };
+	int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));
+	char EXT[] = { (char)(ex & 0XFF), (char)((ex & 0XFF00) >> 8),
+		           (char)((ex & 0XFF0000) >> 16),(char)((ex & 0XFF000000) >> 24), 0 };
 
 	int fps = cap.get(CV_CAP_PROP_FPS);
 	int width = (int)cap.get(CV_CAP_PROP_FRAME_WIDTH);
@@ -364,15 +370,31 @@ void processVideo(string video_name, int ms) {
 	
 	Size S = Size(width, height);
 
-	cout << "Input frame resolution: Width=" << S.width << "  Height=" << S.height
+	cout << "Input video resolution: Width=" << S.width << "  Height=" << S.height
 		<< " of nr#: " << cap.get(CV_CAP_PROP_FRAME_COUNT) << endl;
 	cout << "Input codec type: " << EXT << endl;
 	cout << "FPS = " << fps << endl;
 	
+    //init
 	int interval = 0;
 	bool run = true;
-
 	int frame_number = cap.get(CV_CAP_PROP_FRAME_COUNT);
+		
+	cap >> img;
+	if (!img.data) {
+		cerr << "Unable to load image from video " << video_name << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (img.cols > MAX_WIDTH) {
+		is_resized = true;
+		window_name.append("Cropping app (RESIZED)");
+	}
+	else {
+		window_name.append("Cropping app");
+	}
+	namedWindow(window_name);
+	setMouseCallback(window_name, mouse_callback);
 
 	while (run) {
 		cap >> img;
@@ -382,56 +404,25 @@ void processVideo(string video_name, int ms) {
 			break;
 	    }
 
-	    if (img.cols > 1024) {
-		    is_resized = true;
-		    resized_img.create(Size(img.cols / 2, img.rows / 2), CV_8UC3);
+	    if (is_resized) {
+		    Mat resized_img(Size(img.cols / 2, img.rows / 2), CV_8UC3);
 		    resize(img, resized_img, resized_img.size());
+			img = resized_img.clone();
 	    }
-
-
-
-	    annotated = img.clone();
-
-
-
-	    if(is_resized) {
-		    resized_annotated.create(Size(annotated.cols / 2, annotated.rows / 2), CV_8UC3);
-		    resize(annotated, resized_annotated, resized_annotated.size());
-	    }
-
-	    if (is_resized) {
-		    namedWindow("Cropping app (RESIZED)");
-		    imshow("Cropping app (RESIZED)", resized_annotated);
-		    waitKey(interval);
-	    }
-	    else {
-		    namedWindow("Cropping app");
-		    imshow("Cropping app", annotated);
-		    waitKey(interval);
-	    }
-
-
-	    // Set the mouse event callback function
-	    if (is_resized) {
-		    setMouseCallback("Cropping app (RESIZED)", mouse_callback);
-	    }
-	    else {
-		    setMouseCallback("Cropping app", mouse_callback);
-	    }
-
-	    // Exit by pressing 'q' or esc
 	    
+		imshow(window_name, img);
+		    
 		char key = char(waitKey(interval));
 		if (key == 'q' || key == 27) {
 			run = false;
 		}
-		else if (key == 'p') {
+		else if (key == 'b') { //block
 			interval = 0;
 		}
-		else if (key == 'f') {
+		else if (key == 'p') { //play
 			interval = 30;
 		}
-		else if (key == 's') {
+		else if (key == 's') { //save
 
 			bool saving = true;
 			bool saved = false;
@@ -440,7 +431,6 @@ void processVideo(string video_name, int ms) {
 			ofstream obs_file;
 			
 			while (saving) {
-				//cout << "saving crop image" << endl;
 
 				if (!saved) {
 
@@ -463,82 +453,15 @@ void processVideo(string video_name, int ms) {
 					string saved_crop_name;
 					string saved_image_name;
 
-					if (is_annotated) {
-						Annotation current_ann;
-						current_ann.bounding_box.x = -1000;
-						float min_dist = 0.f;
-						for (std::vector<Annotation>::iterator it = annotations.begin(); it != annotations.end(); ++it) {
-							if (current_ann.bounding_box.x == -1000) {
-								current_ann = *it;
-								min_dist = sqrt(pow(current_ann.bounding_box.x - box.x, 2) + pow(current_ann.bounding_box.y - box.y, 2));
-							}
-							else {
-								float dist = sqrt(pow((*it).bounding_box.x - box.x, 2) + pow((*it).bounding_box.y - box.y, 2));
-								if (dist < min_dist) {
-									current_ann = *it;
-									min_dist = sqrt(pow(current_ann.bounding_box.x - box.x, 2) + pow(current_ann.bounding_box.y - box.y, 2));
-								}
-							}
-
-						}
-
-
-						saved_crop_name.assign(video_name.substr(0, idx) + "_frame_" + n + "_crop_" + box_value + "-" + current_ann.class_name + ".png");
-
-					}
-					else {
-
-						//saved_crop_name.assign(video_name.substr(0, idx) + "_frame_" + n + "_crop_" + box_value + ".png");
-						saved_crop_name.assign(crop_dir + "epl_LEILIV_frame_" + n + "_crop_" + box_value + ".png");
-
-						if (!img_stored) {
-							saved_image_name.assign(whole_dir + "epl_LEILIV_frame_" + n + ".png");
-							
-							string obs_name = whole_dir + "epl_LEILIV_frame_" + n + ".txt";
-							cout << "creating file " << obs_name << endl;
-							obs_file.open(obs_name);
-							if (!obs_file.is_open())
-							{
-								cout << "unable to save object annotations." << endl;
-								exit(EXIT_FAILURE);
-							}
-							else {
-								cout << "file " << obs_name << "created." << endl;
-							}
-						}
-
-					}
-
+					saved_crop_name.assign(video_name.substr(0, idx) + "_frame_" + n + "_crop_" + box_value + ".png");
+					
+					
 					if (imwrite(saved_crop_name, crop)) {
 						cout << "crop: " << saved_crop_name << " saved." << endl;
 					}
 					else {
 						cerr << "Unable to save image: " << saved_crop_name << endl;
 						exit(EXIT_FAILURE);
-					}
-
-					ostringstream ss10;
-					ss10 << box.tl().x;
-					ostringstream ss11;
-					ss11 << box.tl().y;
-					ostringstream ss12;
-					ss12 << box.width;
-					ostringstream ss13;
-					ss13 << box.height;
-					string obj_value = "player " + ss10.str() + " " + ss11.str() + " " + ss12.str() + " " + ss13.str();
-					obs_file << obj_value << endl;
-
-					if (!img_stored) {
-
-						if (imwrite(saved_image_name, img)) {
-							cout << "frame: " << saved_image_name << " saved." << endl;
-						}
-						else {
-							cerr << "Unable to save frame: " << saved_image_name << endl;
-							exit(EXIT_FAILURE);
-						}
-						img_stored = true;
-
 					}
 
 					saved = true;
@@ -559,6 +482,5 @@ void processVideo(string video_name, int ms) {
         frame_number++;
 
 	}
-#endif
 }
 
